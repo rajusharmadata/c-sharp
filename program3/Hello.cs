11d3775d70a7cd7362531fcf3b7288d9e164e464
Raju kumar <rajusharmadata123@gmail.com>
2025-02-11 18:39:39 +0530

    add

---
+++ program3/Hello.cs
@@ -0,0 +1,13 @@
+using System;
+namespace std{
+    class main{
+        public static void Main(string   []args){
+            Console.WriteLine("print name any tghing");
+              Console.WriteLine("print name any tghing");
+                Console.WriteLine("print name any tghing");
+                  Console.WriteLine("print name any tghing");
+            
+
+        }
+    }
+}
