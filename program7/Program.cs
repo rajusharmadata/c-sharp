f91c3dc9fc88271799b24defe74d0e009f0563cc
Raju kumar <rajusharmadata123@gmail.com>
2025-02-20 14:33:00 +0530

    add new files

---
+++ program7/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace std{
+    class program7{
+        static void Main(string [] args){
+
+            // Switch case y
+            // Console.WriteLine("Enter  you day choise  ");
+            // int day = Convert.ToInt32(Console.ReadLine());
+            // switch(day){
+            //     case 1:Console.WriteLine("Monday");
+            //     break;
+
+            //     case 2:Console.WriteLine("Tuesday");
+            //     break;
+
+            //     case 3:Console.WriteLine("Wednesday");
+            //     break;
+
+            //     case 4:Console.WriteLine("Thrusday");
+            //     break;
+                
+            //     case 5:Console.WriteLine("Friday ");
+            //     break;
+
+            //     case 6:Console.WriteLine("Starday");
+            //     break;
+
+            //     case 7:Console.WriteLine("sunday");
+            //     break;
+
+            //     default: Console.WriteLine("invalid choise plz ");
+            //     break;
+           // }
+
+
+        //    loops 
+
+        // 1.While loop?
+            Console.WriteLine("Enter number with you choise :");
+            int i = Convert.ToInt32(Console.ReadLine());
+            while(i<5){
+                Console.WriteLine(i);
+                i++;
+            }
+
+
+        }
+    }
+
+}
