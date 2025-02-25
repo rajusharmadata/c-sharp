60e3048a10be925bb91ca9aa78bee60a1ae16629
Raju kumar <rajusharmadata123@gmail.com>
2025-02-25 14:49:34 +0530

    add new file

---
+++ program13/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;// Language integrated Query
+namespace std
+{
+    class main
+    {
+        // How to handle function overloading 
+        string color = "red";
+        string colorblue = "blue";
+      
+        static void Main(string[] args)
+        {
+            main myobj = new main();
+            main myobj2 = new main();
+            Console.WriteLine(myobj.color);
+            Console.WriteLine(myobj2.colorblue);
+     
+        }
+    }
+}
